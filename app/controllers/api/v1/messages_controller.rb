de4fbf5a825474ad5e06384a7054c6dd9ec5e10d
Yan Siegers <hello@yansiegers.nl>
2022-07-21 12:35:08 +0200

    Generate models and migrations

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::MessagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :messages
+      resources :tags
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
