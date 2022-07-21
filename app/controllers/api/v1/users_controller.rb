e87e83aeb60215a775010788deeab99e7cfb83ec
Yan Siegers <hello@yansiegers.nl>
2022-07-21 13:36:53 +0200

    Add crud for user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,66 @@
+# frozen_string_literal: true
 class Api::V1::UsersController < ApplicationController
   def index
+    users = User.all
+    render json: users, status: :ok
   end
 
   def show
-  end
+    user = User.find_by_id(params[:id])
 
-  def new
+    if user
+      render json: user, status: :ok
+    else
+      payload = { error: 'User not found', status: :bad_request }
+      render json: payload, status: :bad_request
+    end
   end
 
   def create
-  end
+    user = User.new(
+      name: user_params[:name],
+      email: user_params[:email]
+    )
 
-  def edit
+    if user.save
+      render json: user, status: :ok
+    else
+      payload = { error: 'Something went wrong', status: :unprocessable_entity }
+      render json: payload, status: :unprocessable_entity
+    end
   end
 
   def update
+    user = User.find_by_id(params[:id])
+
+    if user&.update(user_params)
+      render json: user, status: :ok
+    elsif user.blank?
+      payload = { error: 'User not found', status: :bad_request }
+      render json: payload, status: :bad_request
+    else
+      payload = { error: 'Something went wrong', status: :unprocessable_entity }
+      render json: payload, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    user = User.find_by_id(params[:id])
+
+    if user&.destroy
+      render json: user, status: :ok
+    elsif user.blank?
+      payload = { error: 'User not found', status: :bad_request }
+      render json: payload, status: :bad_request
+    else
+      payload = { error: 'Something went wrong', status: :unprocessable_entity }
+      render json: payload, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(%i[name email])
   end
 end
